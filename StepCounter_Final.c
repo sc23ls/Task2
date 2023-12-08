450a96dde2a70a6118c839f93a30feb2c9723908
Lauren Spicer <146564951+sc23ls@users.noreply.github.com>
2023-12-08 11:06:37 +0000

    completed a and b

--- StepCounter_Final.c
+++ StepCounter_Final.c
@@ -8,39 +8,43 @@
 // Define any additional variables here
 // Global variables for filename and FITNESS_DATA array
 
-
 // This is your helper function. Do not change it in any way.
 // Inputs: character array representing a row; the delimiter character
 // Ouputs: date character array; time character array; steps character array
 void tokeniseRecord(const char *input, const char *delimiter,
-                    char *date, char *time, char *steps) {
+                    char *date, char *time, char *steps)
+{
     // Create a copy of the input string as strtok modifies the string
     char *inputCopy = strdup(input);
-    
+
     // Tokenize the copied string
     char *token = strtok(inputCopy, delimiter);
-    if (token != NULL) {        strcpy(date, token);
+    if (token != NULL)
+    {
+        strcpy(date, token);
     }
-    
+
     token = strtok(NULL, delimiter);
-    if (token != NULL) {
+    if (token != NULL)
+    {
         strcpy(time, token);
     }
-    
+
     token = strtok(NULL, delimiter);
-    if (token != NULL) {
+    if (token != NULL)
+    {
         strcpy(steps, token);
     }
-    
+
     // Free the duplicated string
     free(inputCopy);
+}
 
-                    }
-
-
-FILE *open_file(char fileName[], char mode[]) {
+FILE *open_file(char fileName[], char mode[])
+{
     FILE *file = fopen(fileName, mode);
-    if (file == NULL) {
+    if (file == NULL)
+    {
         printf("Error: could not open file");
         exit(1);
     }
@@ -48,54 +52,56 @@ FILE *open_file(char fileName[], char mode[]) {
 }
 
 // Complete the main function
-int main() {
+int main()
+{
+    int buffer_size = 100;
+    char line_buffer[buffer_size];
+
+    int i = 0;
     FILE *file;
     char *filename;
-    char *option;
+    char option=' ';
     FITNESS_DATA data[128];
-    printf("Choose from the following options: \n
-    A: Specify the filename to be imported \n
-    B: Display the total number of records in the file \n
-    C: Find the date and time of the timeslot with the fewest steps \n
-    D: Find the data and time of the timeslot with the largest number of steps \n
-    E: Find the mean step count of all the records in the file \n
-    F: Find the longest continuous period where the step count is above 500 steps \n
-    Q: Exit \n");
-    scanf("%s", option);
-    switch(option){
-        case 'A': 
+    while(1){
+        printf("Choose from the following options:\n A: Specify the filename to be imported\n B: Display the total number of records in the file\n C: Find the date and time of the timeslot with the fewest steps\n D: Find the data and time of the timeslot with the largest number of steps\n E: Find the mean step count of all the records in the file\n F: Find the longest continuous period where the step count is above 500 steps\n Q: Exit\n ");
+        scanf(" %c", &option);
+        switch (option)
+        {
+        case 'A':
             printf("Input filename:");
             scanf("%s", filename);
-            file=open_file(filename, "r");
+            file = open_file(filename, "r");
             
-        break;
+            char *delimeter = ", ";
+            while (fgets(line_buffer, buffer_size, file) != NULL)
+            {
+                char *stepsTemp = (char *)malloc(6);
+                char *dateTemp = (char *)malloc(11);
+                char *timeTemp = (char *)malloc(6);
+                tokeniseRecord(line_buffer, delimeter, dateTemp, timeTemp, stepsTemp);
 
+                strcpy(data[i].date, dateTemp);
+                strcpy(data[i].time, timeTemp);
+                data[i].steps = atoi(stepsTemp);
+
+                i++;
+            }
+
+            break;
         case 'B':
-        break;
-
+            printf("Total records: %d\n", i);
+            break;
         case 'C':
-        break;
-
+            break;
         case 'D':
-        break;
-
+            break;
         case 'E':
-        break;
-
+            break;
         case 'F':
-        break;
-
+            break;
         case 'Q':
-        break;
-
+            exit(0);
+            break;
+        }
     }
 }
-
-
-
-
-
-
-
-
-
